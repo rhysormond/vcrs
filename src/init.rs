1d442d5d3bcd79839df1e34cbd02862748f4bb04
rhysormond <git@rhysormond.com>
2020-08-12 16:43:36 -0700

    refactor: clean up init and add todos

--- src/init.rs
+++ src/init.rs
@@ -1,20 +1,20 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::{fs, io};
 
 use crate::repository::Repository;
 
-pub fn init() -> Result<(), Box<dyn Error>> {
+pub fn init() -> Result<(), io::Error> {
     let repo = Repository::new(std::env::current_dir()?);
 
     if !repo.work_tree.read_dir()?.next().is_none() {
-        return Err(Box::new(io::Error::new(
+        return Err(io::Error::new(
             io::ErrorKind::AlreadyExists,
             format!("{:?} is not empty", repo.work_tree),
-        )));
+        ));
     }
 
+    // TODO[Rhys] we need to create things like config, description, tags, etc.
     fs::create_dir_all(repo.objects)?;
     fs::create_dir_all(repo.heads)?;
     let mut file = File::create(repo.head)?;
