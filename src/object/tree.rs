2eb8eb0bd48b6bb9b01b8e1ef4924c8cb6a1557b
rhysormond <git@rhysormond.com>
2020-08-16 16:13:18 -0700

    refactor: deserialize leaves with nom

--- src/object/tree.rs
+++ src/object/tree.rs
@@ -1,7 +1,13 @@
 use std::error::Error;
+use std::num::ParseIntError;
+
+use nom::{
+    bytes::complete::{take, take_while, take_while_m_n},
+    combinator::map_res,
+    IResult,
+};
 
 use crate::object::constant::*;
-use crate::object::util::take_string;
 
 #[derive(Debug, PartialEq)]
 pub struct Tree {
@@ -14,7 +20,7 @@ impl Tree {
     }
 
     pub fn deserialize(body: Vec<u8>) -> Result<Self, Box<dyn Error>> {
-        let mut remainder = body;
+        let mut remainder: &[u8] = body.as_slice();
         let mut leaves: Vec<Leaf> = vec![];
         while !remainder.is_empty() {
             let (leaf, rest) = Leaf::deserialize(remainder)?;
@@ -37,7 +43,7 @@ impl Leaf {
         hex::decode(raw).expect("Unable to encode hash.")
     }
 
-    fn decode_hash(raw: &Vec<u8>) -> String {
+    fn decode_hash(raw: &[u8]) -> String {
         hex::encode(raw)
     }
 
@@ -52,14 +58,33 @@ impl Leaf {
         .concat()
     }
 
-    pub fn deserialize(body: Vec<u8>) -> Result<(Self, Vec<u8>), Box<dyn Error>> {
-        let mut iter = body.iter();
-        let mode = take_string(&mut iter, ASCII_SPACE)?;
-        let path = take_string(&mut iter, ASCII_NULL)?;
-        let hash_raw: Vec<u8> = iter.by_ref().take(20).cloned().collect();
-        let hash = Self::decode_hash(&hash_raw);
-        let remainder: Vec<u8> = iter.cloned().collect();
+    fn deserialize_mode(input: &[u8]) -> IResult<&[u8], String> {
+        // NOTE[Rhys] modes don't have to be 6 characters as leading 0s are dropped
+        map_res(take_while_m_n(0, 6, |c| c != ASCII_SPACE), |c: &[u8]| {
+            String::from_utf8(c.to_vec())
+        })(input)
+    }
 
+    fn deserialize_path(input: &[u8]) -> IResult<&[u8], String> {
+        map_res(take_while(|c| c != ASCII_NULL), |c: &[u8]| {
+            String::from_utf8(c.to_vec())
+        })(input)
+    }
+
+    fn deserialize_hash(input: &[u8]) -> IResult<&[u8], String> {
+        // Note[Rhys] this ParseIntError is a lie but nom expects there to be some error type here
+        map_res(take(20 as usize), |h: &[u8]| {
+            Ok::<String, ParseIntError>(Leaf::decode_hash(h))
+        })(input)
+    }
+
+    pub fn deserialize(body: &[u8]) -> Result<(Self, &[u8]), Box<dyn Error>> {
+        let remainder = body;
+        let (remainder, mode) = Leaf::deserialize_mode(remainder).unwrap();
+        // Note[Rhys] we slice remainder here to drop the space delimiter
+        let (remainder, path) = Leaf::deserialize_path(&remainder[1..]).unwrap();
+        // Note[Rhys] we slice remainder here to drop the null delimiter
+        let (remainder, hash) = Leaf::deserialize_hash(&remainder[1..]).unwrap();
         Ok((Self { mode, path, hash }, remainder))
     }
 }
@@ -90,6 +115,42 @@ mod tests {
     }
 
     #[test]
+    fn deserializes_mode() {
+        let raw = [49, 48, 48, 54, 52, 52, ASCII_SPACE];
+        let (remainder, mode) = Leaf::deserialize_mode(&raw).unwrap();
+        assert_eq!(mode, "100644");
+        assert_eq!(remainder, [ASCII_SPACE]);
+    }
+
+    #[test]
+    fn deserializes_mode_with_implicit_leading_zero() {
+        let raw = [52, 48, 48, 48, 48, ASCII_SPACE];
+        let (remainder, mode) = Leaf::deserialize_mode(&raw).unwrap();
+        assert_eq!(mode, "40000");
+        assert_eq!(remainder, [ASCII_SPACE]);
+    }
+
+    #[test]
+    fn deserializes_path() {
+        let raw = [82, 69, 65, 68, 77, 69, 46, 109, 100, ASCII_NULL];
+        let (remainder, mode) = Leaf::deserialize_path(&raw).unwrap();
+        assert_eq!(mode, "README.md");
+        assert_eq!(remainder, [ASCII_NULL]);
+    }
+
+    #[test]
+    fn deserializes_hash() {
+        let test_char: u8 = 100;
+        let raw: Vec<u8> = vec![
+            0, 219, 250, 237, 236, 71, 165, 169, 35, 228, 150, 70, 108, 63, 223, 76, 200, 117, 247,
+            74, test_char,
+        ];
+        let (remainder, mode) = Leaf::deserialize_hash(&raw).unwrap();
+        assert_eq!(mode, "00dbfaedec47a5a923e496466c3fdf4cc875f74a");
+        assert_eq!(remainder, [test_char]);
+    }
+
+    #[test]
     fn round_trips_leaf() {
         let mode: Vec<u8> = vec![49, 48, 48, 54, 52, 52];
         let path: Vec<u8> = vec![46, 103, 105, 116, 105, 103, 110, 111, 114, 101];
@@ -107,7 +168,7 @@ mod tests {
             path: String::from(".gitignore"),
             hash: String::from("ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba"),
         };
-        let (leaf, remainder) = Leaf::deserialize(serialized.clone()).unwrap();
+        let (leaf, remainder) = Leaf::deserialize(&serialized).unwrap();
         assert!(remainder.is_empty());
         assert_eq!(leaf, deserialized);
         assert_eq!(leaf.serialize(), serialized)
